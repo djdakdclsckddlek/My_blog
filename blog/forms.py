63ac7b2df814dca72016a26dec7af0557dc2e0c9
[KIMCHANGHWAN] <98susckdghks@naver.com>
2023-10-29 19:19:25 +0900

    모바일 메뉴바 수정,  글쓰기 폼에서 썸네일 이미지, 파일 업로드 추가

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'thumbnail', 'file_upload']
+
+    thumbnail = forms.ImageField(label='Thumbnail', required=False)
+    file_upload = forms.FileField(label='File_field', required=False)
